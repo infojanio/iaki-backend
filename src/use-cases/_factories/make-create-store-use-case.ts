ecb4f05c1c672ea658d053997f6721b475aa7864
Janio Florencio dos Santos <info.janio@gmail.com>
2025-09-30 13:03:58 -0300

    rahdar_oficial

--- src/use-cases/_factories/make-create-store-use-case.ts
+++ src/use-cases/_factories/make-create-store-use-case.ts
@@ -1,13 +1,10 @@
-import { PrismaStoresRepository } from '@/repositories/prisma/prisma-stores-repository'
-import { CreateStoreUseCase } from '../stores/create-store'
-import { PrismaAddressesRepository } from '@/repositories/prisma/prisma-addresses-repository'
+import { PrismaStoresRepository } from "@/repositories/prisma/prisma-stores-repository";
+import { RegisterUseCase } from "../stores/create-store";
+import { PrismaAddressesRepository } from "@/repositories/prisma/prisma-addresses-repository";
 export function makeCreateStoreUseCase() {
-  const storesRepository = new PrismaStoresRepository()
-  const addressesRepository = new PrismaAddressesRepository()
+  const storesRepository = new PrismaStoresRepository();
+  const addressesRepository = new PrismaAddressesRepository();
 
-  const createStoreUseCase = new CreateStoreUseCase(
-    storesRepository,
-    addressesRepository,
-  )
-  return createStoreUseCase
+  const createStoreUseCase = new RegisterUseCase(storesRepository);
+  return createStoreUseCase;
 }
